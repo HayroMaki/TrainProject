0676c5db291137dedae61fc5d3b35380beaf396c
jerem <jeremyea9zg3@outlook.fr>
2025-03-21 17:52:30 +0100

    Made the months appear dynamically

--- src/calendar/CalendarContent.tsx
+++ src/calendar/CalendarContent.tsx
@@ -1,8 +1,47 @@
 export const CalendarContent = (props: {
     month: number;
+    year: number;
 }) => {
 
+    const options = { weekday: "short" }
+    const startdate = new Date(props.year, props.month, 1);
+    const enddate = new Date(props.year, props.month + 1, -1);
 
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    const startday = Intl.DateTimeFormat("fr-FR", options).format(startdate);
+    const endday = enddate.getDate() + 1;
+
+    const days = ["lun.", "mar.", "mer.", "jeu.", "ven.", "sam.", "dim."];
+
+    const calendarDays = [];
+    let isPassed = false;
+    let monthDay = 1;
+    let week: (number|null)[] = [];
+
+    for (let i = 1; i <= 42; i++) {
+        if (! (isPassed || startday === "lun.")) {
+            // The month hasn't started yet
+            if (days[i % 7] === startday) {
+                isPassed = true;
+            }
+            week.push(null);
+            continue;
+        } else if (monthDay > endday) {
+            // The month is exceeded
+            week.push(null);
+        } else {
+            // The month started
+            week.push(monthDay);
+            monthDay++;
+        }
+
+        if (i % 7 === 0) {
+            // A new week started
+            calendarDays.push(week);
+            week = []
+        }
+    }
 
     return (
         <table>
@@ -13,56 +52,28 @@ export const CalendarContent = (props: {
                     <th>Mer</th>
                     <th>Jeu</th>
                     <th>Ven</th>
-                    <th>Sa</th>
-                    <th>Di</th>
+                    <th>Sam</th>
+                    <th>Dim</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>2</td>
-                    <td>3</td>
-                    <td>4</td>
-                    <td>5</td>
-                    <td>6</td>
-                    <td>7</td>
-                </tr>
-                <tr>
-                    <td>8</td>
-                    <td>9</td>
-                    <td>10</td>
-                    <td>11</td>
-                    <td>12</td>
-                    <td>13</td>
-                    <td>14</td>
-                </tr>
-                <tr>
-                    <td>15</td>
-                    <td>16</td>
-                    <td>17</td>
-                    <td>18</td>
-                    <td>19</td>
-                    <td>20</td>
-                    <td>21</td>
-                </tr>
-                <tr>
-                    <td>22</td>
-                    <td>23</td>
-                    <td>24</td>
-                    <td>25</td>
-                    <td>26</td>
-                    <td>27</td>
-                    <td>28</td>
-                </tr>
-                <tr>
-                    <td>29</td>
-                    <td>30</td>
-                    <td>31</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
+            {
+                calendarDays.map((week, i) => {
+                    return (
+                        <tr key={i}>
+                            {
+                                week.map((day, j) => {
+                                    return (
+                                        <td key={j}>
+                                            {day}
+                                        </td>
+                                    )
+                                })
+                            }
+                        </tr>
+                    )
+                })
+            }
             </tbody>
         </table>
     )
