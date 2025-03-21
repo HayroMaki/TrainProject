eccbbaaac38e80fdd10cf540e973da8c2e557678
jerem <jeremyea9zg3@outlook.fr>
2025-03-21 16:36:07 +0100

    Static calendar with a modifiable month name

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,8 @@
-import { useState } from "react";
+import {CalendarDisplayer} from "./calendar/CalendarDisplayer.tsx";
 
 function App() {
-    const [message, setMessage] = useState<string>("");
-
-    const insertClient = async (name: string, age: number) => {
-        if (name == null || ! age) {
-            console.log("data error");
-            return;
-        }
-        const response = await fetch("http://localhost:5000/api/insertClient", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ name: name, age: age }),
-        });
-
-        const data = await response.json();
-        setMessage(data.message);
-    };
-
     return (
-        <form onSubmit={event => {
-            event.preventDefault();
-
-            const name = document.getElementById("name") as HTMLInputElement;
-            const ageInput = document.getElementById("age") as HTMLInputElement;
-            const age = parseInt(ageInput.value);
-            insertClient(name.value, age);
-        }}>
-            <input type={"text"} id={"name"} required />
-            <input type={"number"} id={"age"} required />
-            <button type={"submit"}>Insérer</button>
-            <p>{message}</p>
-        </form>
+        <CalendarDisplayer />
     );
 }
 
