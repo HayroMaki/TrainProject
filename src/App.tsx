6535a42d50a856f5fedcf6f34b6bfbaab9cc9b73
jerem <jeremyea9zg3@outlook.fr>
2025-03-22 17:50:08 +0100

    Divided Connection.tsx to be in 2 different pages (Connection and Inscription) and modified the header to link to the connection page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import {Route, HashRouter as Router, Routes } from 'react-router-dom';
 
 import {Connection} from "./pages/Connection.tsx";
+import {Inscription} from "./pages/Inscription.tsx";
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Connection />}/>
+                <Route path="/Connection" element={<Connection />}/>
+                <Route path="/Inscription" element={<Inscription />}/>
             </Routes>
         </Router>
     );
