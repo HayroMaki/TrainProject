35247d9b45d408cd4829a08d0a726af7c44b586a
jerem <jeremyea9zg3@outlook.fr>
2025-03-22 17:13:58 +0100

    Added a header (pretty much empty for now) Added a connection / inscription page (that doesn't do anything for now)

--- src/pages/Connection.tsx
+++ src/pages/Connection.tsx
@@ -1,10 +1,41 @@
-import Form from "../components/form/Form.tsx";
+import React, {useState} from "react";
 
-const Connection = () => {
+export const Connection = () => {
+
+    const [isInscription, setIsInscription] = useState(false);
+
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+    }
+
     return (
-        <>
-            <Form/>
-        </>
+        <form className="Connection-form" onSubmit={handleSubmit}>
+            <h2 className="Connection-form-title">Formulaire { isInscription ? "d'inscription" : "de connexion" }</h2>
+            <div className="Connection-form-container">
+                <label className="Connection-form-label" htmlFor="email">Adresse mail</label>
+                <input className="Connection-form-input" type="email" id="email" placeholder="exemple@mail.com" />
+            </div>
+            { isInscription &&
+                <div className="Connection-form-container">
+                    <label className="Connection-form-label" htmlFor="confirmEmail">Confirmer votre adresse mail</label>
+                    <input className="Connection-form-input" type="email" id="confirmEmail" placeholder="exemple@mail.com" />
+                </div>
+            }
+            <div className="Connection-form-container">
+                <label className="Connection-form-label" htmlFor="password">Mot de passe</label>
+                <input className="Connection-form-input" type="password" id="password" />
+            </div>
+            <button className="Connection-form-submit" type="submit">Submit</button>
+            { isInscription ?
+                <span className="Inscription-text">
+                    Vous avez un compte ?<br/>
+                    Connectez-vous maintenant <span className="Inscription-link" onClick={() => setIsInscription(false)}>ici</span>
+                </span> :
+                <span className="Inscription-text">
+                    Pas de compte ?<br/>
+                    Inscrivez-vous maintenant <span className="Inscription-link" onClick={() => setIsInscription(true)}>ici</span>
+                </span>
+            }
+        </form>
     )
 }
-export default Connection;
