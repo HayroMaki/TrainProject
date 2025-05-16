c66684fa7940884801e38e2452ecd0124d3f2791
Jules <julesgrange@outlook.fr>
2025-05-16 20:32:10 +0200

    deleted train interface as unused

--- src/interfaces/Train.tsx
+++
@@ -1,9 +0,0 @@
-import {Option} from "./Option.tsx";
-
-export default interface Train {
-    train_ref: string;
-    wagons: number[];
-    restaurant_wagon: number;
-    floors: number;
-    available_options: Option[];
-}
